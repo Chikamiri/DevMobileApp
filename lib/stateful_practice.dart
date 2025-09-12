577dbb7b854c75ba88ba08942e6496ea41e10552
Chikamiri <bach.px@outlook.com>
2025-09-12 10:09:01 +0700

    Homepage 2 + fix

--- lib/stateful_practice.dart
+++ lib/stateful_practice.dart
@@ -17,7 +17,6 @@ class _BankState extends State<Bank> {
     });
   }
 
-  // danh sách trang dựa trên state
   List<Widget> _pages() => [
     _homepage(
       currency: _currency,
@@ -34,52 +33,49 @@ class _BankState extends State<Bank> {
     return Scaffold(
       backgroundColor: const Color(0xFFE5E1E1),
       body: _pages()[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        fixedColor: const Color(0xFF005EFF),
-        type: BottomNavigationBarType.fixed,
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Padding(
-              padding: EdgeInsets.only(top: 5),
-              child: Icon(Icons.home),
-            ),
-            label: '',
+      bottomNavigationBar: BottomAppBar(
+        padding: EdgeInsets.zero,
+        notchMargin: 0,
+        color: Colors.white,
+        child: SizedBox(
+          height: 56,
+          child: Row(
+            children: [
+              Expanded(child: _navIcon(Icons.home, 0)),
+              Expanded(child: _navIcon(Icons.pie_chart, 1)),
+
+              //QR
+              Container(
+                width: 76,
+                height: double.infinity,
+                decoration: BoxDecoration(
+                  color: Color(0xFF005EFF),
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: InkWell(
+                  onTap: () => _onItemTapped(2),
+                  child: const Icon(
+                    Icons.qr_code,
+                    color: Colors.white,
+                    size: 28,
+                  ),
+                ),
+              ),
+
+              Expanded(child: _navIcon(Icons.chat_bubble_rounded, 3)),
+              Expanded(child: _navIcon(Icons.person, 4)),
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: Padding(
-              padding: EdgeInsets.only(top: 5, right: 30),
-              child: Icon(Icons.pie_chart),
-            ),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Padding(
-              padding: EdgeInsets.only(top: 5, left: 30),
-              child: Icon(Icons.chat_bubble),
-            ),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Padding(
-              padding: EdgeInsets.only(top: 5),
-              child: Icon(Icons.person),
-            ),
-            label: '',
-          ),
-        ],
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: const Color(0xFF005EFF),
-        onPressed: () {
-          setState(() {
-            _selectedIndex = 2;
-          });
-        },
-        child: const Icon(Icons.qr_code, color: Colors.white),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+
+  Widget _navIcon(IconData icon, int index) {
+    return IconButton(
+      icon: Icon(icon),
+      color: _selectedIndex == index ? const Color(0xFF005EFF) : Colors.grey,
+      onPressed: () => _onItemTapped(index),
     );
   }
 }
@@ -109,14 +105,12 @@ Widget _homepage({
         Stack(
           clipBehavior: Clip.none,
           children: [
-            // Header
             Container(
               padding: const EdgeInsets.all(16),
               decoration: const BoxDecoration(color: Color(0xFF005EFF)),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  // search
                   Row(
                     children: [
                       IconButton(
@@ -135,7 +129,7 @@ Widget _homepage({
                               color: Colors.white,
                             ),
                             filled: true,
-                            fillColor: Colors.white.withOpacity(0.2),
+                            fillColor: Colors.white.withValues(alpha: 0.2),
                             hintStyle: const TextStyle(color: Colors.white),
                             border: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(12),
@@ -154,8 +148,6 @@ Widget _homepage({
                       ),
                     ],
                   ),
-
-                  // balance
                   const SizedBox(height: 24),
                   Center(
                     child: Column(
@@ -242,8 +234,6 @@ Widget _homepage({
                 ],
               ),
             ),
-
-            // Action buttons
             Positioned(
               bottom: -40,
               left: 16,
@@ -262,17 +252,33 @@ Widget _homepage({
                   ],
                 ),
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: const [
                     _ActionButton(
                       icon: Icons.send,
                       label: "Send",
                       color: Colors.blue,
                     ),
+                    SizedBox(
+                      height: 40,
+                      child: VerticalDivider(
+                        color: Colors.black26,
+                        thickness: 1,
+                        width: 10,
+                      ),
+                    ),
                     _ActionButton(
                       icon: Icons.request_page,
                       label: "Request",
                       color: Colors.orange,
+                    ),
+                    SizedBox(
+                      height: 40,
+                      child: VerticalDivider(
+                        color: Colors.black26,
+                        thickness: 1,
+                        width: 10,
+                      ),
                     ),
                     _ActionButton(
                       icon: Icons.account_balance,
@@ -285,29 +291,88 @@ Widget _homepage({
             ),
           ],
         ),
-
-        const SizedBox(height: 60), // để transaction không bị đè
-        // Transaction
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16),
-          child: Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              "Transaction",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
+        const SizedBox(height: 60),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                "Transaction",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              IconButton(
+                icon: const Icon(Icons.arrow_forward),
+                onPressed: () {},
+              ),
+            ],
           ),
         ),
         const SizedBox(height: 8),
-        _transactionItem(Icons.credit_card, "Spending", "-\$500", Colors.red),
-        _transactionItem(
-          Icons.monetization_on,
-          "Income",
-          "\$3000",
-          Colors.green,
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 16),
+          padding: const EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(12),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withValues(alpha: 0.1),
+                blurRadius: 6,
+                offset: const Offset(0, 3),
+              ),
+            ],
+          ),
+          child: ListView(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            children: [
+              _transactionItem(
+                Icons.credit_card,
+                "Spending",
+                "-\$500",
+                Colors.red,
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.keyboard_arrow_right),
+                ),
+              ),
+              Divider(color: Colors.grey.withValues(alpha: 0.3)),
+              _transactionItem(
+                Icons.monetization_on,
+                "Income",
+                "\$3000",
+                Colors.green,
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.keyboard_arrow_right),
+                ),
+              ),
+              Divider(color: Colors.grey.withValues(alpha: 0.3)),
+              _transactionItem(
+                Icons.receipt_long,
+                "Bills",
+                "-\$800",
+                Colors.red,
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.keyboard_arrow_right),
+                ),
+              ),
+              Divider(color: Colors.grey.withValues(alpha: 0.3)),
+              _transactionItem(
+                Icons.savings,
+                "Savings",
+                "\$1000",
+                Colors.orange,
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.keyboard_arrow_right),
+                ),
+              ),
+            ],
+          ),
         ),
-        _transactionItem(Icons.receipt_long, "Bills", "-\$800", Colors.red),
-        _transactionItem(Icons.savings, "Savings", "\$1000", Colors.orange),
       ],
     ),
   );
@@ -329,7 +394,7 @@ class _ActionButton extends StatelessWidget {
     return Column(
       children: [
         CircleAvatar(
-          backgroundColor: color.withOpacity(0.2),
+          backgroundColor: color.withValues(alpha: 0.2),
           child: Icon(icon, color: color),
         ),
         const SizedBox(height: 6),
@@ -344,16 +409,36 @@ Widget _transactionItem(
   String title,
   String amount,
   Color color,
+  IconButton iconButton,
 ) {
-  return ListTile(
-    leading: CircleAvatar(
-      backgroundColor: color.withOpacity(0.2),
-      child: Icon(icon, color: color),
-    ),
-    title: Text(title),
-    trailing: Text(
-      amount,
-      style: TextStyle(color: color, fontWeight: FontWeight.bold),
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 8),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            const SizedBox(width: 8),
+            CircleAvatar(
+              backgroundColor: color.withValues(alpha: 0.2),
+              child: Icon(icon, color: color),
+            ),
+            const SizedBox(width: 12),
+            Text(title, style: const TextStyle(fontSize: 16)),
+          ],
+        ),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              amount,
+              style: TextStyle(color: color, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(width: 8),
+            iconButton,
+          ],
+        ),
+      ],
     ),
   );
 }
