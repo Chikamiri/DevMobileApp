e75d575c32d091e0ba4871275fa9b81dfe71e35d
Chikamiri <bach.px@outlook.com>
2025-09-26 07:43:57 +0700

    Another organizing stuff

---
+++ lib/learn/student_management/student_managerment.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'internal/internal_student_management.dart';
+
+class StudentManagerment extends StatelessWidget {
+  const StudentManagerment({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Quản lý sinh viên'),
+        backgroundColor: Colors.deepOrange,
+      ),
+      body: const InternalStudentManagerment(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,20 @@
-import 'package:app/student_managerment.dart';
+import 'package:app/learn/student_management/student_managerment.dart';
 import 'package:flutter/material.dart';
 
 //import 'card_practice.dart';
 //import 'stateful_practice.dart';
 //import 'house_info.dart';
-import 'student_managerment.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-<<<<<<< HEAD
       home: SafeArea(
         child: Scaffold(
           //   ),
           body: StudentManagerment(),
         ),
       ),
-=======
-      home: SafeArea(child: Scaffold(body: StudentManagerment())),
->>>>>>> 818f0f0b92fa694ee863fa8492cd54be5744f15c
     ),
   );
 }
